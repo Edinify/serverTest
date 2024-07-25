5a5829bc449ab6ecdec0092ab9dea254b118cf0a
SamedovResul <samedovrasul7@gmail.com>
2024-07-25 12:06:07 +0400

    /

--- server.js
+++ server.js
@@ -1,45 +1,34 @@
-const fs = require('fs').promises;
-const exists = require('fs').exists;
-const path = require('path');
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
-const express = require('express');
-const bodyParser = require('body-parser');
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT;
+const uri = process.env.DB_URI;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use(express.static('public'));
-app.use('/feedback', express.static('feedback'));
-
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'pages', 'feedback.html');
-  res.sendFile(filePath);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/exists', (req, res) => {
-  const filePath = path.join(__dirname, 'pages', 'exists.html');
-  res.sendFile(filePath);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
 
-app.post('/create', async (req, res) => {
-  const title = req.body.title;
-  const content = req.body.text;
-
-  const adjTitle = title.toLowerCase();
-
-  const tempFilePath = path.join(__dirname, 'temp', adjTitle + '.txt');
-  const finalFilePath = path.join(__dirname, 'feedback', adjTitle + '.txt');
-
-  await fs.writeFile(tempFilePath, content);
-  exists(finalFilePath, async (exists) => {
-    if (exists) {
-      res.redirect('/exists');
-    } else {
-      await fs.rename(tempFilePath, finalFilePath);
-      res.redirect('/');
-    }
-  });
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.listen(80);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
