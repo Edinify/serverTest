dd9e806ac21b578ab26f5a77eb70daaeccd5d8c0
SamedovResul <samedovrasul7@gmail.com>
2024-07-26 14:29:31 +0400

    /

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 
 const Goal = require('./models/goal');
 
